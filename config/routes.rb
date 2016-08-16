8e8a81ca86caf314b7ce87383afceb37a4bfabfc
Amy Chan <chamy09@gmail.com>
2016-08-15 20:34:34 -0500

    added profile CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,13 @@ root to: 'posts#landing_page'
 
 get '/' => 'homes#index'
 
-
 get '/profiles' => 'profiles#index'
+get '/profile/new' => 'profiles#new'
+post '/profile/' => 'profiles#create'
 get '/profile/:id' => 'profiles#show'
+get '/profile/:id/edit' => 'profiles#edit'
+patch '/profile/:id' => 'profiles#update'
+delete '/profile/:id' => 'profiles#destroy'
 
 get '/blogs' => 'posts#index'
 get '/blog/:id' => 'posts#show'
@@ -22,7 +26,5 @@ get '/books' => 'books#index'
 get '/book/:id' => 'books#show'
 
 get '/logout' => 'auth0#logout'
-
-
 
 end
