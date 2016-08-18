1a8168091669b7281a8320e1b619d7bac4aef817
Troy Leach <troyleach29@gmail.com>
2016-08-17 22:08:43 -0500

    added spec and spec helper files deleted miniTest

---
+++ spec/controllers/homes_controller_test.rb
@@ -0,0 +1,12 @@
+require_re '../test_helper'
+
+class HomesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+  describe 'this is a fake test' do
+    it 'expect test to fail' do
+      expect(true).to eq(false)
+    end
+  end
+end
