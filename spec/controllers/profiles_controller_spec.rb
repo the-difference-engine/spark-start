955ce3c704fcbb16588bd951f219df57ed5136e3
Amy Chan <chamy09@gmail.com>
2016-08-22 20:34:14 -0500

    add rspec tests for profile controller

--- spec/controllers/profiles_controller_spec.rb
+++ spec/controllers/profiles_controller_spec.rb
@@ -2,5 +2,24 @@ require 'rails_helper'
 
 RSpec.describe ProfilesController, type: :controller do
 
+  describe 'this is a fake test' do
+    it 'expect test to fail' do
+      expect(true).to eq(false)
+    end
+  end
+
+  describe 'GET #show' do
+    it 'assigns the requested profile to @profile' do
+      profile = create(:profile)
+      get :show, id: profile
+      expect(assigns(:profile)).to eq profile
+    end
+
+    it 'renders the :show template' do
+      profile = create(:profile)
+      get :show, id: profile
+      expect(response).to render_template :show
+    end
+  end
 
 end
