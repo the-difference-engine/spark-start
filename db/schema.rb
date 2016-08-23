9ba144101bb971774b008311fb2c2f5c128cf1c9
Amy Chan <chamy09@gmail.com>
2016-08-22 19:44:36 -0500

    add more attributes to profile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160812005911) do
+ActiveRecord::Schema.define(version: 20160823002224) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,10 @@ ActiveRecord::Schema.define(version: 20160812005911) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "state"
+    t.string   "city"
   end
 
 end
