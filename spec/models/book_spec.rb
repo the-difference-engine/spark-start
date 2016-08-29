69895c7c98773a72a85adcf5e6f0322e55d6e5d5
stefanvlahov <stefan.vlahov5@gmail.com>
2016-08-29 18:41:02 -0500

    Indented code for model association testing

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -2,8 +2,8 @@ require "rails_helper"
 
 RSpec.describe Book, type: :model do
 
-describe 'Database data types' do
-  it { should have_db_column(:url).of_type(:string) }
-  it { should have_db_column(:user_id).of_type(:integer) }
-end
+  describe 'Database data types' do
+    it { should have_db_column(:url).of_type(:string) }
+    it { should have_db_column(:user_id).of_type(:integer) }
+  end
 end
