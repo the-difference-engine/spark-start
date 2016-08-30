3572471d8787ebd1f930b583fcc81f918d449df7
capricornication <terrence.q.park@gmail.com>
2016-08-29 20:33:42 -0500

    added figaro & application.yml

--- Gemfile
+++ Gemfile
@@ -27,7 +27,7 @@ gem 'jbuilder', '~> 2.5'
 # gem 'redis', '~> 3.0'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
-
+gem "figaro"
 gem 'omniauth', '~> 1.3.1'
 gem 'omniauth-auth0', '~> 1.4.1'
 
--- config/initializers/auth0.rb
+++ config/initializers/auth0.rb
@@ -1,9 +1,9 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider(
     :auth0,
-    '120SH91pK3Jo2SIUNxPW3PU688IcVu83',
-    'FCMobloXpFRp3AsDp4cqPEXagOHnK9qpUmnHJydskaVbUHUvhBbGqs_9DgMY3dUe',
-    'sparkstart.auth0.com',
+    Auth0.clientid = ENV["clientid"],
+    Auth0.clientsecret = ENV["clientsecret"],
+    Auth0.domain = ENV["domain"],
     callback_path: "/auth/auth0/callback"
   )
 end
