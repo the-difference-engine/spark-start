11f0ab194594bf218f88b0fad1d6fb93ad4ca228
capricornication <terrence.q.park@gmail.com>
2016-08-29 20:49:08 -0500

    corrected auth0/figaro issue

--- config/initializers/auth0.rb
+++ config/initializers/auth0.rb
@@ -1,9 +1,9 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider(
     :auth0,
-    Auth0.clientid = ENV["clientid"],
-    Auth0.clientsecret = ENV["clientsecret"],
-    Auth0.domain = ENV["domain"],
+    ENV["clientid"],
+    ENV["clientsecret"],
+    ENV["domain"],
     callback_path: "/auth/auth0/callback"
   )
 end
