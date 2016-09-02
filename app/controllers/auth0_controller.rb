2d84de44da8048ba3a7349a6370822614cc0f6fb
capricornication <terrence.q.park@gmail.com>
2016-09-01 21:02:40 -0500

    added user model and untested create user
    create new user works
    
    redirect updated
    
    refactored per code review

--- app/controllers/auth0_controller.rb
+++ app/controllers/auth0_controller.rb
@@ -1,16 +1,30 @@
 class Auth0Controller < ApplicationController
   def callback
-    # This stores all the user information that came from Auth0
-    # and the IdP
-
+    
     session[:userinfo] = request.env['omniauth.auth']
+    user_info = session[:userinfo]
+    
+    params[:token] = user_info.credentials["token"]
+    params[:email] = user_info.info["email"]
 
-    # Redirect to the URL you want after successfull auth
-    redirect_to '/blogs'
+    users = User.all
+    if users.find_by_email(params[:email]).nil?
+      new_user
+    else
+      redirect_to blogs_path
+    end
+    
   end
 
-  def user
-    @user = session[:userinfo]
+  def new_user
+      @user = User.new(user_params) 
+      if @user.save
+        flash[:success] = "Welcome To Spark Start"
+        redirect_to profile_new_path
+      else
+        flash[:error] = "Try Again"
+        render root
+      end
   end
 
   def logout
@@ -23,4 +37,11 @@ class Auth0Controller < ApplicationController
     # show a failure page or redirect to an error page
     @error_msg = request.params['message']
   end
+
+  private
+
+    def user_params
+      params.permit(:token, :email)
+    end
+
 end
