f78bfed93a19636182eea51c73210ade2d3fac05
Amy Chan <chamy09@gmail.com>
2016-09-26 21:35:07 -0500

    remove user id from comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,20 +1,26 @@
 class CommentsController < ApplicationController
 
   def create
+<<<<<<< c3e0880c73e8504750c524a48ccb3c5f6a1e5453
 <<<<<<< e1f8c1d12567e7581b7e301a425438e5cc10b7a0
 =======
     @user_session = session[:userinfo]
     @user_token = @user_session["extra"]["raw_info"]["identities"][0]["user_id"]
 
 >>>>>>> attempt to add commenter user name
+=======
+>>>>>>> remove user id from comments
     @post = Post.find(params[:post_id])
     @comment = Comment.create(
       body: params[:body],
       post_id: @post.id,
+<<<<<<< c3e0880c73e8504750c524a48ccb3c5f6a1e5453
 <<<<<<< e1f8c1d12567e7581b7e301a425438e5cc10b7a0
 =======
       user_id: @user_token
 >>>>>>> attempt to add commenter user name
+=======
+>>>>>>> remove user id from comments
     )
 
     redirect_to request.referer
