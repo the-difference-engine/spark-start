dd108d855abecde00d292ef7183bd1750ce30074
Amy Chan <chamy09@gmail.com>
2016-09-28 18:36:52 -0500

    add blog posts to homes index

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -4,8 +4,8 @@ def index
   if session[:userinfo].present?
     @current_user = User.find_by_token(session[:userinfo]["extra"]["raw_info"]["identities"][0]["user_id"])
   end
-    #@user = session[:userinfo]
 
+  @user = session[:userinfo]
   @posts = Post.all.sort_by(&:created_at).take(3)
 
 end
