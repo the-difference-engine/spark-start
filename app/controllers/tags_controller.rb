2f0b74be0a417c6e0234ff2ebffb0f1d1ec50a4e
Amy Chan <chamy09@gmail.com>
2016-10-06 20:06:27 -0500

    add checkboxes for categories

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,11 +1,9 @@
 class TagsController < ApplicationController
 
   def index
+
     @tags = Tag.all
-    respond_to do |format|
-      format.html
-      format.json { render :json => @tags }
-    end
+    
   end
 
 end
