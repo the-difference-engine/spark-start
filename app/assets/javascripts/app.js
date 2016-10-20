43b2648dca2eeeb4ae149c6ad5f3998900ae50b5
Amy Chan <chamy09@gmail.com>
2016-10-20 18:15:38 -0500

    started working on new tag submit

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,3 +1,16 @@
+<<<<<<< d01b303b692c08116b405fb00bfabc69961ce635
+=======
+// var inputFields = new Vue({
+//   el: '#inputFields',
+//   data: {
+//     title: '',
+//     author: '',
+//     body: ''
+//   }
+// });
+
+
+>>>>>>> started working on new tag submit
 var tags =  new Vue({
   el: '#tags',
   data: {
@@ -12,19 +25,31 @@ var tags =  new Vue({
       url: '/tags.json',
       success: function(res) {
         that.tags = res;
+        // console.log(that.tags)
       }
     })
   },
   methods: {
     addTag: function () {
+<<<<<<< d01b303b692c08116b405fb00bfabc69961ce635
       this.checkedTags.push('');
     },
     deleteTagField: function (checkedTag) {
       this.checkedTags.$remove(checkedTag)
+=======
+      this.checkedTags.push({ 
+        tag_name: '' 
+      });
+      console.log(this.checkedTags)
+>>>>>>> started working on new tag submit
     }
   }
 });
 
+<<<<<<< d01b303b692c08116b405fb00bfabc69961ce635
+=======
+
+>>>>>>> started working on new tag submit
 var categories = new Vue ({
   el: '#categories',
   data: {
@@ -50,4 +75,74 @@ var categories = new Vue ({
       this.checkedCategories.$remove(checkedCategory)
     }
   }
+<<<<<<< d01b303b692c08116b405fb00bfabc69961ce635
+=======
+});
+
+
+var tagsCategories = new Vue({
+  el: '#tagsCategories',
+  data: {
+    title: '',
+    author: '',
+    body: '',
+    tags: [],
+    checkedTags: [],
+    categories: [],
+    checkedCategories: []
+  },
+  methods: {
+    // postTags: function(){
+    //   var that = this;
+    //   $.ajax({
+    //     method: 'POST',
+    //     data: {
+    //       checkedTags: that.checkedTags
+    //     },
+    //     url: '/tags.json',
+    //     success: function(res) {
+    //       that.errors = {};
+    //       that.checkedTags.push(res);
+    //     }
+    //   })
+    // },
+    // postCategories: function(){
+    //   var that = this;
+    //   $.ajax({
+    //     method: 'POST',
+    //     data: {
+    //       checkedCategories: that.checkedCategories
+    //     },
+    //     url: '/categories.json',
+    //     success: function(res) {
+    //       that.errors = {};
+    //       that.checkedCategories.push(res);
+    //     }
+    //   })
+    // },
+    // postFields: function(){
+    //   var that = this;
+    //   $.ajax({
+    //     method: 'POST',
+    //     data: {
+    //       title: that.title,
+    //       author: that.author,
+    //       body: that.body
+    //     },
+    //     url: '/post.json',
+    //     success: function(res) {
+    //       that.errors = {};
+    //       that.title.push(res);
+    //       that.author.push(res);
+    //       that.body.push(res);
+    //     }
+    //   })
+    // },
+    // handler: function(){
+    //   this.postCategories;
+    //   this.postTags;
+    //   this.postFields;
+    // }
+  }
+>>>>>>> started working on new tag submit
 });
