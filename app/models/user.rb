a0a0e33150909b7b96964f3fc60e50a374a07a68
JenWestfallen <chicago.author@yahoo.com>
2016-10-27 20:43:49 -0500

    correct Paperclip columns to book vs user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,5 @@ class User < ApplicationRecord
   # has_many :posts
   has_many :chats
   has_one :profile
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161028004836) do
+ActiveRecord::Schema.define(version: 20161028013503) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "books", force: :cascade do |t|
     t.string   "url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
     t.string   "user_id"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   create_table "categories", force: :cascade do |t|
@@ -90,14 +94,10 @@ ActiveRecord::Schema.define(version: 20161028004836) do
   create_table "users", force: :cascade do |t|
     t.string   "role"
     t.string   "token"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
     t.string   "email"
-    t.boolean  "admin",               default: false
-    t.string   "avatar_file_name"
-    t.string   "avatar_content_type"
-    t.integer  "avatar_file_size"
-    t.datetime "avatar_updated_at"
+    t.boolean  "admin",      default: false
   end
 
 end
