b5f6295a5bf5220aa095ebc0119e1023fdd30fda
jleev <justinviegelahn@gmail.com>
2016-10-27 20:55:12 -0500

    updated header and footer and created three new pages

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,6 +2,9 @@ class BooksController < ApplicationController
 	before_filter :logged_in_using_omniauth?
 
 def index
+	if session[:userinfo].present?
+		@current_user = User.find_by_token(session[:userinfo]["extra"]["raw_info"]["identities"][0]["user_id"])
+	end
 end
 
 def show
--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -10,6 +10,24 @@ def index
 
 end
 
+def how
+  if session[:userinfo].present?
+    @current_user = User.find_by_token(session[:userinfo]["extra"]["raw_info"]["identities"][0]["user_id"])
+  end
+end
+
+def mission
+  if session[:userinfo].present?
+    @current_user = User.find_by_token(session[:userinfo]["extra"]["raw_info"]["identities"][0]["user_id"])
+  end
+end
+
+def videos
+  if session[:userinfo].present?
+    @current_user = User.find_by_token(session[:userinfo]["extra"]["raw_info"]["identities"][0]["user_id"])
+  end
+end
+
 def theme
 
 end
