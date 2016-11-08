d09524a6f9b3b450cab12f37f420f7336f61de02
Amy Chan <chamy09@gmail.com>
2016-11-07 20:45:27 -0600

    added js to assets rb

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,5 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+Rails.application.config.assets.precompile += ['vue.js', 'vue-router.js', 'vue-resource.js', 'jquery.js', 'bootstrap-sprockets.js', 'jquery_ujs.js', 'jquery.min.js', 'jquery-migrate.min.js', 'jquery.easing.min.js', 'bootstrap.min.js', 'bootstrap-hover-dropdown.min.js', 'raphael-min.js', 'livicons-1.4.min.js', 'raphael-min.js', 'jquery.waypoints.min.js', 'jquery.counterup.min.js', 'owl.carousel.min.js', 'jquery.flexslider-min.js', 'jquery.appear.js', 'modernizr.custom.97074.js', 'jquery.stellar.min.js', 'jquery.BlackAndWhite.js', 'lightbox.min.js', 'jquery.hoverdir.js', 'jquery.sticky.js', 'wow.min.js', 'headroom.js', 'versa.js', 'masterslider.min.js', 'jquery.cubeportfolio.min.js', 'tinymce.js']
