c2ea389ce7f8edd373854d5dad40b4301e61cc31
Brennantwalsh <brennanthomaswalsh@gmail.com>
2016-12-05 20:57:33 -0600

    tests tag taggedpost and category as well removed bad change

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,5 @@ class User < ApplicationRecord
   has_many :chats
   has_one :profile
 
-  def self.find_user
-    find_by_token(session[:userinfo]["extra"]["raw_info"]["identities"][0]["user_id"])
-  end
+
 end
