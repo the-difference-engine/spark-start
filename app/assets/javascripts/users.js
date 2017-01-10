18f348ad91fd8a036fe3e5104f3b2d7f4117bd35
Brennantwalsh <brennanthomaswalsh@gmail.com>
2017-01-09 20:58:43 -0600

    button change removed the old bootstrap button

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,3 +1,18 @@
 function changeRole() {
-  
+  var url = "/api/v1/users/3";
+  var data = {"admin": false}
+
+  $.ajax(url, {
+      method: 'PUT',
+      contentType: 'application/json',
+      processData: false,
+      data: JSON.stringify({
+          user_id: data
+      })
+  });
+
+};
+
+function colorChange() {
+  var checked
 };
