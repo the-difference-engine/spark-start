a24cd49b6e5841ad5bbfd8d23407fc5fada7d96f
Brennantwalsh <brennanthomaswalsh@gmail.com>
2017-01-12 19:32:02 -0600

    button to change role works

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def update
-    user = User.find(params(:id))
-    user.update(admin: params(:admin))
+    user = User.find(params[:id])
+    user.update(admin: params[:admin])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,6 @@ Rails.application.routes.draw do
 
   get '/users' => 'users#index'
 
-
   namespace :api do 
     namespace :v1 do 
       get '/users' => 'users#index'
