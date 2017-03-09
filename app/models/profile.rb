c970e46bac3fe4c094c542715b044db417a59e1d
Troy Leach <troyleach29@gmail.com>
2017-03-09 17:49:44 -0600

    wip trying to fix profile comment out all validates

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,13 +1,13 @@
 class Profile < ApplicationRecord
 
-  validates :experience, presence: true, numericality: true
-  validates :bio, presence: true
-  validates :phone, presence: true
-  validates :career, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :state, presence: true, length: { maximum: 2 }
-  validates :city, presence: true
+  # validates :experience, presence: true, numericality: true
+  # validates :bio, presence: true
+  # validates :phone, presence: true
+  # validates :career, presence: true
+  # validates :first_name, presence: true
+  # validates :last_name, presence: true
+  # validates :state, presence: true, length: { maximum: 2 }
+  # validates :city, presence: true
 
   belongs_to :user
 
