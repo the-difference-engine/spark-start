c795396ca98cab057bd8169895572c1a181114fc
Troy Leach <troyleach29@gmail.com>
2017-03-09 19:46:24 -0600

    reverted back to the org paperclip produciont.rb file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,26 +85,14 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.paperclip_defaults = {
-    :storage => :s3,
-    :path => "sparkstart/:id/avatar/:style.:extension",
-    :bucket => "graymatterapplication",
-    :s3_host_name => "s3-us-west-2.amazonaws.com",
-    :default_url => "https://s3-us-west-2.amazonaws.com/graymatterapplication/default/generic-avatar.jpg",
-    :s3_region => "us-west-2",
-    :s3_credentials => {
-      :access_key_id => ENV["AWS_ACCESS_KEY_ID"],
-      :secret_access_key => ENV["AWS_SECRET_ACCESS_KEY"]
-    }
+  storage: :s3,
+  :path => "sparkstart/:id/avatar/:style.:extension",
+  :s3_host_name => "s3-us-west-2.amazonaws.com",
+  s3_credentials: {
+    bucket: ENV.fetch('S3_BUCKET_NAME'),
+    access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+    s3_region: ENV.fetch('AWS_REGION'),
   }
-#   config.paperclip_defaults = {
-#   storage: :s3,
-#   :path => "sparkstart/:id/avatar/:style.:extension",
-#   :s3_host_name => "s3-us-west-2.amazonaws.com",
-#   s3_credentials: {
-#     bucket: ENV.fetch('S3_BUCKET_NAME'),
-#     access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-#     secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-#     s3_region: ENV.fetch('AWS_REGION'),
-#   }
-# }
+}
 end
