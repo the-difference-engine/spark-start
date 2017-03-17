8f0a89dfbfda7abadff2d681e6f1baf9ccc516b2
Alexander Dulchinos <adulchinos@gmail.com>
2017-03-16 20:50:38 -0500

    made html pages for eventual admin page tabs

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: tags
+#
+#  id         :integer          not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  tag_name   :string
+#
+
 class Tag < ApplicationRecord
 
   validates :tag_name, uniqueness: true
--- spec/models/chat_spec.rb
+++ spec/models/chat_spec.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: chats
+#
+#  id         :integer          not null, primary key
+#  content    :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :integer
+#
+
 require "rails_helper"
 
 RSpec.describe Chat, type: :model do
