139e7ac8def0ba69db910d3ee819bb5365c3c161
Troy Leach <troyleach29@gmail.com>
2017-04-24 20:12:54 -0500

    modified the theme action

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -5,8 +5,6 @@ def index
     @current_user = User.find_by_token(session[:userinfo]["extra"]["raw_info"]["identities"][0]["user_id"])
   end
 
-  @user = session[:userinfo]
-  @posts = Post.all.sort_by(&:created_at).reverse!.take(3)
 
 end
 
@@ -29,6 +27,7 @@ def videos
 end
 
 def theme
+  # delete this code, only for review
 
 end
 
