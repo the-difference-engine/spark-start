1983cc47b04495b72e000635e30627d02c2a1357
yo-no <spencerblake91@gmail.com>
2017-06-12 11:19:39 -0500

    added validations for change book and change image

--- app/models/book.rb
+++ app/models/book.rb
@@ -28,31 +28,38 @@ class Book < ApplicationRecord
   has_many :categories, through: :categories_author_books
   #has_many :authors, through: :authors_books
   has_attached_file :cover, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-  validates_attachment_content_type :cover, content_type: /\Aimage\/.*\z/ 
+  validates_attachment_content_type :cover, content_type: /\Aimage\/.*\z/, message: " must be an image file."
   has_attached_file :ebook, styles: {thumbnail: "60x60"}
-  validates_attachment :ebook, content_type: { content_type: "application/pdf" }
+  validates_attachment :ebook, content_type: { content_type: "application/pdf", message: "file must be a pdf." }
+  after_validation :clean_paperclip_errors
 
-def authors_names
-  self.authors.map { |authors| authors.full_name }  
 
-  # authors = Author.where(book_id = book)
+  def authors_names
+    self.authors.map { |authors| authors.full_name }  
 
-  # authors.each do |author|
-    
-  # end
-end
+    # authors = Author.where(book_id = book)
 
-def pdf_exists?
-  if self.ebook.url == "/ebooks/original/missing.png"
-    return "Not Available."
-  else
-    return self.ebook.url
+    # authors.each do |author|
+      
+    # end
   end
-end
 
-def check_downloads
-  self.book_downloads >= self.max_downloads
-end
+  def pdf_exists?
+    if self.ebook.url == "/ebooks/original/missing.png"
+      return "Not Available."
+    else
+      return self.ebook.url
+    end
+  end
+
+  def check_downloads
+    self.book_downloads >= self.max_downloads
+  end
+
+  def clean_paperclip_errors
+    errors.delete(:cover)
+    errors.delete(:ebook_content_type)
+  end
 
 
 end
