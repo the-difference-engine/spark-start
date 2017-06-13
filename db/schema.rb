663c2958124e4582a0d9a318624180e8dbf842ad
acolletti21 <ashleymcolletti@gmail.com>
2017-06-12 20:59:17 -0500

    added questions to book table and started angular for tabs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170519012557) do
+ActiveRecord::Schema.define(version: 20170613012637) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,9 @@ ActiveRecord::Schema.define(version: 20170519012557) do
     t.datetime "ebook_updated_at"
     t.integer  "book_downloads",     default: 0
     t.integer  "max_downloads"
+    t.string   "question_1"
+    t.string   "question_2"
+    t.string   "question_3"
   end
 
   create_table "categories", force: :cascade do |t|
@@ -122,6 +125,13 @@ ActiveRecord::Schema.define(version: 20170519012557) do
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
     t.string   "linkedin_url"
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.integer  "book_id"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "tagged_posts", force: :cascade do |t|
