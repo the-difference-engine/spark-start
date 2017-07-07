6c739bcb727c6e322efcb8b909f02891e3fff3d9
yo-no <spencerblake91@gmail.com>
2017-07-06 19:18:48 -0500

    added some angular to book show page

--- app/assets/javascripts/books_ctrl.js
+++ app/assets/javascripts/books_ctrl.js
@@ -2,30 +2,37 @@
   "use strict";
 
   angular.module("app").controller("booksCtrl", function($scope){
-$scope.test = "testing"
+    $scope.test = "testing"
+
+    $scope.showQuestion = function(question) {
+        $scope.questionSelected = true;
+        console.log(question);
+    };
 
 
-    $scope.tabs = [
-            {title: 'Question One',
-            url: 'one.tpl.html'
-        }, {
-            title: 'Question Two',
-            url: 'two.tpl.html'
-        }, {
-            title: 'Question Three',
-            url: 'q3.tpl.html'
-        }
-            ];
 
-    $scope.currentTab = 'one.tpl.html';
 
-    $scope.onClickTab = function(tab) {
-        $scope.currentTab = tab.url;
-    }
+    // $scope.tabs = [
+    //         {title: 'Question One',
+    //         url: 'one.tpl.html'
+    //     }, {
+    //         title: 'Question Two',
+    //         url: 'two.tpl.html'
+    //     }, {
+    //         title: 'Question Three',
+    //         url: 'q3.tpl.html'
+    //     }
+    //         ];
+
+    // $scope.currentTab = 'one.tpl.html';
+
+    // $scope.onClickTab = function(tab) {
+    //     $scope.currentTab = tab.url;
+    // }
     
-    $scope.isActiveTab = function(tabUrl) {
-        return tabUrl == $scope.currentTab;
-    }
+    // $scope.isActiveTab = function(tabUrl) {
+    //     return tabUrl == $scope.currentTab;
+    // }
 
     window.scope = $scope;
   });
--- app/assets/javascripts/flash.js
+++ app/assets/javascripts/flash.js
@@ -1,6 +1,6 @@
 //make flash message disappear after 5 seconds
-$('document').ready(function() {
-  setTimeout(function() {
-    $('.alert').slideUp();
-  }, 5000);
-});
+// $('document').ready(function() {
+//   setTimeout(function() {
+//     $('.alert').slideUp();
+//   }, 5000);
+// });
