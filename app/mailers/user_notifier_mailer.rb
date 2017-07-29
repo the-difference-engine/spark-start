082ce08c51f38a957a727a8d4997dab342f8684c
Troy Leach <troyleach29@gmail.com>
2017-07-28 22:50:52 -0500

    Updated the welcome email

--- app/mailers/user_notifier_mailer.rb
+++ app/mailers/user_notifier_mailer.rb
@@ -1,14 +1,13 @@
 class UserNotifierMailer < ApplicationMailer
+  default :from => 'company@sparkstart.org'
 
-
-	default :from => 'company@sparkstart.org'
-
-	# send a signup email to the user, pass in the user object that   contains the user's email address
-	def send_signup_email(user)
-	  @user = user
-	  mail( :to => @user.email,
-	  :subject => 'Thanks for signing up for Spark Start!' )
-	end
-
-
+  # send a signup email to the user, pass in the user object that   contains the user's email address
+  def send_signup_email(user)
+    @user = user
+    attachments.inline['newlogo.png'] = File.read('app/assets/images/newlogo.png')
+    mail( :to => @user.email,
+         :subject => 'Thanks for signing up for Spark Start!' )
+  end
 end
+
+
